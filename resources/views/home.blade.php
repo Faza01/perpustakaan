4751635a65760108dba34d45877ae278f0b7713f
Faza01 <fazabilwidi@gmail.com>
2023-03-17 02:56:26 +0700

    ukk

---
+++ resources/views/home.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.master')
+
+@section('topbar')
+    @include('part.navbar')
+@endsection
+
+@section('judul')
+    <h1 class="text-dark">Daftar Buku</h1>
+@endsection
+
+@section('content')
+
+<form class="navbar-search mb-3" action="/" method="GET">
+<div class="input-group">
+    <input type="search" name="search" class="form-control bg-light border-1 small" placeholder="Cari Judul Buku"
+        aria-label="Search" aria-describedby="basic-addon2" style="border-color: #3f51b5;">
+    <div class="input-group-append">
+        <button class="btn btn-primary" type="submit">
+            <i class="fas fa-search fa-sm"></i>
+        </button>
+    </div>
+</div>
+</form>
+
+<div class="card container-fluid mb-3">
+
+<div class="row d-flex flex-wrap justify-content-center">
+
+    @forelse ($buku as $item)
+        <div class="col-auto my-2" style="width:18rem;">
+            <div class="card mx-2 my-2" style="min-height:28rem;">
+                @if ($item->gambar != null)
+                    <img class="card-img-top" style="max-height:300px;" src="{{ asset('/images/' . $item->gambar) }}">
+                @else
+                    <img class="card-img-top" style="height:200px;" src="{{ asset('/images/noImage.jpg') }}">
+                @endif
+                <div class="card-body d-flex flex-column justify-content-between">
+                    <div class="detai-buku">
+                        <h5 class="card-title text-dark" style="text-align: center"><a
+                                href="/buku/{{ $item->id }}"style="text-decoration: none; font-size:1rem;font-weight:bold;">
+                                {{ $item->judul }}</a></h5>
+                        <p class = "cart-text m-0">Kode Buku : {{ $item->kode_buku }}</p>
+                        <p class="card-text m-0">Pengarang : <a href="/buku/{{ $item->id }}"
+                                style="text-decoration: none;">{{ $item->pengarang }}</a></p>
+                        <p class="card-text m-0">Kategori : </p>
+                        <p class="text-primary">
+                            @foreach ($item->kategori_buku as $kategori )
+                            {{ $kategori->nama, }},
+                            @endforeach
+                    </p>
+                        <p class="card-text m-0">Status : {{$item->status  }}</p>
+                    </div>
+
+                    <div class="button-area">
+                        <button class="btn-sm btn-info px-2"> <a href="/buku/{{ $item->id }}"
+                        style="text-decoration: none; color:white;">Detail</a></button>
+                    </div>
+
+                    <!--Delete Modal -->
+                    <div class="modal fade" id="DeleteModal{{ $item->id }}" tabindex="-1" role="dialog"
+                        aria-labelledby="ModalLabelDelete" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="ModalLabelDelete">Ohh No!</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Are you sure you want to delete?</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-outline-primary"
+                                        data-dismiss="modal">Cancel</button>
+                                    <form action="/buku/{{ $item->id }}" method="post">
+                                        @csrf
+                                        @method('delete')
+                                        <button class="btn btn-outline-danger px-4" type="submit"
+                                            value="delete">Delete</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    @empty
+        <h1 class="text-primary mt-3">Tidak ada buku</h1>
+    @endforelse
+
+</div>
+
+<div class="d-flex justify-content-between mx-2 my-2">
+    <p class="text-primary my-2">Menampilkan {{ $buku->currentPage() }} dari {{ $buku->lastPage() }} Halaman</p>
+
+    {{ $buku->links() }}
+</div>
+
+</div>
+@endsection
---
+++ resources/views/part/navbar.blade.php
@@ -0,0 +1,52 @@
+<nav class="navbar navbar-expand navbar-light bg-navbar topbar mb-4 static-top" style="background: linear-gradient(to right, #72e9e3, #0ed2f7);">
+    <div class="container-fluid">
+        <a class="navbar-brand mx-3" href="">
+            Perpustakaan
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav me-auto">
+                
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto   ">
+                <!-- Authentication Links -->
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+                      aria-haspopup="true" aria-expanded="false">
+                      <span class="d-none d-lg-inline">{{Auth::user()->name}}</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                      <a class="dropdown-item" href="/profile">
+                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                        Profile
+                      </a>
+                      <a class="dropdown-item" href="{{route('logout')}}" data-toggle="modal" data-target="#logoutModal">
+                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                        Logout
+                      </a>
+                    </div>
+                  </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
